88ddb3c110672a143fd208396693b917083f3f25
ShiinaMinatus <xij1111@gmail.com>
2014-07-03 15:00:22 +0800

    Signed-off-by: ShiinaMinatus <xij1111@gmail.com>

---
+++ Model/companyInfoModel.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of companyInfoModel
+ *
+ * @author Administrator
+ */
+class companyInfoModel  extends Basic{
+
+    public function __construct() {
+
+        $this->child_name = 'company_info';
+
+        parent::__construct();
+    }
+    
+    
+
+}
+
+?>
